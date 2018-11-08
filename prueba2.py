17a12e7b771ccef040c1b835629bcbb571bc27b0
mariano10792 <mariano10792@gmail.com>
2018-11-08 19:54:27 -0300

    Se encuentran los picos, pero se requiere optimizacion

--- prueba2.py
+++ prueba2.py
@@ -21,20 +21,15 @@ skPixTree = f.Get('skPixTree')
 
 skPixTree.SetBranchStatus("pix",0)
 skPixTree.SetBranchStatus('pix',1)
-
+rows=400*50
 for i in range(1):
     pixValue = []
-    x = []
-    # print x
-    for j in range(19999):
-        x = np.append(x,j)
-        #print x
     #print 1
     for index, event in enumerate(skPixTree):
         #
-        #  if index==10000:
+        if index==rows:
         #print i, index
-        #     break
+                break
         #
         #     # time.sleep(2)
         #
@@ -60,21 +55,31 @@ for i in range(1):
     range1=250000
     range2=550000
     bin=(range2-range1)/100
-    n,bins,patches = plt.hist(pixValue,bins=bin,range=(range1,range2))
-    # plt.figure(1)
-    #:plt.axis([-300,10000,0,1000
-    indexes = peakutils.indexes(smooth(n,5), thres=0.02/max(smooth(n,5)), min_dist= bin)
-    peaks= peakutils.interpolate(np.linspace(range1,range2,bin), smooth(n,5), ind=indexes, width=1) #uses a gaussian function and the precedent indexes to enhance our peak finding
-    print indexes
-    print peaks
-    peaks = [int(i) for i in peaks]
-    print peaks
+    n1,bins1,patches1 = plt.hist(pixValue,bins=bin,range=(range1,range2))
+    index1 = peakutils.indexes(smooth(n1,5), thres=0.02/max(smooth(n1,5)), min_dist= bin)
+    peak1= peakutils.interpolate(np.linspace(range1,range2,bin), smooth(n1,5), ind=index1, width=1) #uses a gaussian function and the precedent indexes to enhance our peak finding
+    #print indexes
+    #print peaks
+    #peaks = [int(i) for i in peaks]
+    #print peaks
          # plt.show()
     plt.figure(2)
-
-    #plt.plot(n)
+    peak1 = [int(i) for i in peak1]
+    range1=peak1[0]-int(np.sqrt(peak1[0]))*4
+    range2=peak1[0]+int(np.sqrt(peak1[0]))*4
+    bin=(range2-range1)/20
+    n,bins,patches = plt.hist(pixValue,bins=bin,range=(range1,range2))
+    indexes = peakutils.indexes(n, thres=0.02/max(smooth(n,20)), min_dist= 170*bin/(range2-range1))
+    peaks= peakutils.interpolate(np.linspace(range1,range2,bin), n, ind=indexes, width=1) #uses a gaussian function and the precedent indexes to enhance our peak finding
+    print indexes
+    peaks = [int(i) for i in peaks]
+    print peaks
+    print peaks
     plt.plot(np.linspace(range1,range2,bin),smooth(n,5))
     # print n[indexes]
     plt.plot(peaks,n[indexes],'bo')
-    plt.show()
+    #plt.show()
+    x = np.linspace(1,len(peaks),len(peaks))
+    slope = np.polyfit(x,peaks,1)
+    print slope
     i+=1
