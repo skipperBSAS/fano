d5664eebfc594a1b86fba9a24879d11a5400a6f8
mariano10792 <mariano10792@gmail.com>
2018-11-20 16:00:31 -0300

    en este branch tengo el find peaks que itera en bins

--- findPeaks.py
+++ findPeaks.py
@@ -9,6 +9,7 @@ import sys
 import time
 inroot = sys.argv[1]
 
+
 # ===================================================
 # Funcion para "suavizar"
 # ===================================================
@@ -89,34 +90,39 @@ for i in range(1):
     # Buscamos la media y sigma de cada pico de carga
     # ===================================================
 
-    plt.figure(2)
+
 
     peak1 = [double(i) for i in peak1]
     range1=peak1[0]-int(np.sqrt(peak1[0]))*10
     range2=peak1[0]+int(np.sqrt(peak1[0]))*10
     BIN=int((range2-range1)/30) # enlarging the dividend, gives us thicker bins
-    N=30
+    N=101
     bin_vector=np.linspace(BIN-N/2,BIN+N/2, num=N)
     m=0
-    chi =[0]*N
-    dist_peaks =[0]*N
+    delta =[0]*N
+    dist_peaks =((),)
 
     #chi=np.linspace(BIN-N/2,BIN+N/2, num=N)
     for h in bin_vector:
         #j=j+1
         print m
-        n,bins,patches = plt.hist(pixValue,bins=bin,range=(range1,range2))
-        indexes = peakutils.indexes(n, thres=0.02/max(smooth(n,20)), min_dist= 150*bin/(range2-range1))
-        peaks= peakutils.interpolate(np.linspace(range1,range2,bin), n, ind=indexes, width=1) #uses a gaussian function and the precedent indexes to enhance our peak finding
-        #print indexes
+        #print "bines = "+str(bin_vector[m])
+        plt.figure(m+2)
+        n,bins,patches = plt.hist(pixValue,bins=bin_vector[m],range=(range1,range2))
+        indexes = peakutils.indexes(n, thres=0.02/max(smooth(n,20)), min_dist= 150*bin_vector[m]/(range2-range1))
+
+        peaks= peakutils.interpolate(np.linspace(range1,range2,bin_vector[m]), n, ind=indexes, width=1) #uses a gaussian function and the precedent indexes to enhance our peak finding
+
+        print len(n)
+        print len(bins)
+        print len(patches)
         peaks = [double(i) for i in peaks]
-        M= np.linspace(1,len(peaks)-1,len(peaks)-1)
+        #M= np.linspace(1,len(peaks)-1,len(peaks)-1)
         
-        print len(peaks)
-        dist_peaks=[0 for i in M]
-        print len(dist_peaks)
-        dist_peaks=[peaks[i]-peaks[i-1] for i in M]
-        print len(dist_peaks)
+        #print len(peaks)
+        dist_peaks=np.diff(peaks)
+        #dist_peaks=[peaks[i+1]-peaks[i] for i in range(len(peaks)-1)]
+        #print len(dist_peaks)
         #print np.mean(dist_peaks)
         x = np.linspace(1,len(peaks),len(peaks))
         range= np.linspace(range1,range2,len(peaks))
@@ -124,25 +130,31 @@ for i in range(1):
         x1=np.linspace(range1,range2)
         y1=slope[0]*x+slope[1]
         media=np.mean(dist_peaks)
-        chi[m]=slope[0]-media
-        #chi[i]=1
-        m=m+1
-        if m!=0:
-            if chi[m]<chi[m-1]:
+        delta[m]=abs(slope[0]-media)
+        #print "slope = "+str(slope[0])
+        #print "media = "+str(media)
+        if m==0:
+	    factor=slope[0]
+            factor2=np.mean(dist_peaks)
+	    delta2=delta[m]
+
+ 
+        else:
+            if delta[m]<delta2:
                 factor=slope[0]
                 factor2=np.mean(dist_peaks)
-        else:
-            factor=slope[0]
-            factor2=np.mean(dist_peaks)
-
-    print factor
-    plt.plot(np.linspace(range1,range2,bin),smooth(n,5))
+		delta2=delta[m]
+	    	M=m
+	    	
+	m=m+1
+    print str(factor)+" delta = "+str(delta2)
+#    plt.plot(np.linspace(range1,range2,bin_vector[M]),smooth(n,5))
     # print n[indexes]
-    plt.plot(peaks,n[indexes],'bo')
+    #plt.plot(peaks,n[indexes],'bo')
     #plt.show()
-    plt.figure(3)
-    plt.plot(x,peaks,'.')
-    plt.plot(x,y1)
+#    plt.figure(3)
+#    plt.plot(x,peaks,'.')
+#    plt.plot(x,y1)
 
-    plt.figure(4)
-    plt.hist(dist_peaks)
+#    plt.figure(4)
+#    plt.hist(dist_peaks)
